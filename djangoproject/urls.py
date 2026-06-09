dc386ae318ee1a6aa06373454035022bb3eae19d
keziah Irene <kezi2804@gmail.com>
2026-06-09 18:04:19 +0530

    modified urls file

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  
+from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('course_registration.urls')),  
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
